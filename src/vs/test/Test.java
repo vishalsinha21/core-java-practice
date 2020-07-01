cb760f6acfb8c91f636ab464f546504c290020cb
Vishal Sinha <vishal.sinha21@gmail.com>
2020-07-01 18:38:02 +0200

    simulate creating index

--- src/vs/test/Test.java
+++ src/vs/test/Test.java
@@ -1,6 +1,5 @@
 package vs.test;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 public class Test {
@@ -9,5 +8,6 @@ public class Test {
     public static void main(String args[]) {
 
         System.out.println(new Date(2020, 12, 31).getTime());
+        System.out.println("create index");
     }
 }
